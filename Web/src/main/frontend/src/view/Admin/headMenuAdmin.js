d0bd348165d01cdf0483c43a5f4c74ef954ea33f
pjy7814 <pjy7814@naver.com>
2022-11-08 20:05:00 +0900

    admin Menu Bar

--- Web/src/main/frontend/src/view/Admin/headMenuAdmin.js
+++ Web/src/main/frontend/src/view/Admin/headMenuAdmin.js
@@ -1,6 +1,7 @@
 import {Link} from "react-router-dom";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import {Button, Col, Row} from "antd";
 
 export default function HeadMenuAdmin() {
 
@@ -23,19 +24,24 @@ export default function HeadMenuAdmin() {
 
 
     }
+
     return(
-        <nav>
-            <Link to='/admin'>Home</Link>
-            <br />
-            <Link to='/infoAdmin'>Info</Link>
-            <br />
-            <Link to='/productAdmin'>Product</Link>
-            <br />
-            <Link to='/noticeAdmin'>Notice</Link>
-            <br />
-            <Link to='/contactAdmin'>Contact</Link>
-            <br />
-            <button type="submit" onClick={onClick}>로그아웃</button>
-        </nav>
+        <>
+            <Row>
+                <Col flex="auto"></Col>
+                <Col flex="50px"><Link type="submit" onClick={onClick}>Logout</Link></Col>
+
+            </Row>
+
+            <Row justify="end">
+                <Col span={2}><Button type="text" size="middle"><Link to='/'>Home</Link></Button></Col>
+                <Col span={2}><Button type="text" size="middle"><Link to='/infoAdmin'>Information</Link></Button></Col>
+                <Col span={2}><Button type="text" size="middle"><Link to='/productAdmin'>Product</Link></Button></Col>
+                <Col span={2}><Button type="text" size="middle"><Link to='/noticeAdmin'>Notice</Link></Button></Col>
+                <Col span={2}><Button type="text" size="middle"><Link to='/contactAdmin'>Contact</Link></Button></Col>
+            </Row>
+
+        </>
+
     );
 }
